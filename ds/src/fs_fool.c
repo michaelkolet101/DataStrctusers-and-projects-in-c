088c51418dcd1ce07e005437ccaf2d75c5eee862
Michael <michael24kolet@gmail.com>
2022-01-16 14:44:43 +0200

    update

--- ds/src/fs_fool.c
+++ ds/src/fs_fool.c
@@ -4,21 +4,23 @@
 struct fixed_size_pool
 {
     size_t free_chunks;
-    void *next_to_free;
+    size_t chunk_size;
+    size_t next_to_free;
 } ;
 
 fsp_ty *FSPoolInit(void *memory, size_t memory_size, size_t chunk_size)
 {
     /*
-    1. Defines a variable i = 0 
-        We define a new memory = memory
+    1. Defines a variable i = 1 type of fixed_size_pool
+        We define a new_fixed_size_pool = memory
         We will initialize the free_chunks and next_to_free.
-    2. Loop from to size of all memory Divisions of cunks - 1.
+        We will initialize the chunk_size
+    2. Loop from to size of (all memory Divisions of cunks) - 1.
     3. Initialize each index in the last set of ballads that
                                     will indicate the next index.
     4. Initialize the last index in an array that points to NULL.
-    5. The member who represents the top of the list in the .
-        structure will point to the first free Icades which is Incodes 0
+    5. The member who represents the top of the list in the 
+        structure will point to the first free Icades which is Incodes 1.
     */
 }
 
@@ -28,7 +30,7 @@ void *FSPoolAlloc(fsp_ty *fs_pool)
     1. Sets a pointer and compares it to NULL for the return value "ret val".
     2. Check if there is a space that can be allocated to the user.
     3. If yes then the pointer for the return value 
-                            will point to the first available index.
+                            will point to the fs_pool + OFFSETOF[next_to_free].
     4. The next place to release will be the 
                             indexing dereference of what we send to the user.
     5. free_chunks -= 1.
