a30c4a4ae358e7d1dd6b618532753ed18d00c548
Michael <michael24kolet@gmail.com>
2022-01-17 11:36:03 +0200

    fix

--- ds/include/fs_fool.h
+++ ds/include/fs_fool.h
@@ -1,5 +1,13 @@
 /*  API changelog
 
+Version 1.2
+	*Added informative Notes to following functions:
+		- FSPoolInit - usage notes, failure and performance notes
+		- FSPoolFree - undefined case.
+		- FSPoolAlloc - failure notes.
+		- FSPoolCalcSize - usage notes.
+	*Added Time Complexities to all functions.
+
 Version 1.1
 	* FSPoolCountFree was updated to FSPoolCountFreeChunks
 	* in function FSPoolInit the argument element_size was changed to chunk_size
@@ -18,28 +26,31 @@ Version 1.0
 typedef struct fixed_size_pool fsp_ty;
 
 
-/* Notes:  
-* Time Complexity: */
+/* 	Notes: memory_size should be acquired by calling FSPoolCalcSize.
+		   Data should be aligned to word size by the user, otherwise 
+		   performance may be diminished, in some systems a failure may occur.
+		   chunk_size should be word size at the very least. 
+* 	Time Complexity: O(n) - memory_size / chunk_size */
 fsp_ty *FSPoolInit(void *memory, size_t memory_size, size_t chunk_size);
 
 
-/* Notes: 
-* Time Complexity: */
+/* 	Notes: returns NULL pointer in case of fully allocated pool.
+	Time Complexity: O(1) */
 void *FSPoolAlloc(fsp_ty *fs_pool);
 
 
-/* Notes: 
-Time Complexity:*/  
+/* 	Notes: if sent chunk to free is not in range - undefined behaviour
+Time Complexity: O(1) */  
 void FSPoolFree(fsp_ty *fs_pool, void *chunk_to_free);
 
 
-/* Notes: 
-* Time Complexity: */
+/* 	Notes: 
+	Time Complexity: O(n) */
 size_t FSPoolCountFreeChunks(fsp_ty *fs_pool);
 
 
-/* Notes: 
-* Time Complexity: */
+/* 	Notes: chunk_size should be word size at the very least.
+	Time Complexity: O(1) */
 size_t FSPoolCalcSize(size_t num_chunks, size_t chunk_size);
 
 
--- ds/src/fs_fool.c
+++ ds/src/fs_fool.c
@@ -1,66 +1,141 @@
+#include<assert.h>
+#include<stddef.h>
+#include<limits.h>
 #include <stdio.h>
 #include "fs_fool.h"
 
+static Alignment(chunk_size);
+
 struct fixed_size_pool
 {
-    size_t free_chunks;
-    size_t chunk_size;
-    size_t next_to_free;
+    size_t first_block;
+    /*DEBUG_ONLY(size_t size_mem);*/
 } ;
 
+
+ /* need to normalize chunk_size to be word aligned */
+    
+    /*  use start of memory to keep fsp ds metadata  
+        initialize metadata:    */
+
+    /*  initialize each freelist's index content with the index of the next one
+        the final index points to size_t MAX.
+        eager initialization */
 fsp_ty *FSPoolInit(void *memory, size_t memory_size, size_t chunk_size)
 {
-    /*
-    1. Defines a variable i = 1 type of fixed_size_pool
-        We define a new_fixed_size_pool = memory
-        We will initialize the free_chunks and next_to_free.
-        We will initialize the chunk_size
-    2. Loop from to size of (all memory Divisions of cunks) - 1.
-    3. Initialize each index in the last set of ballads that
-                                    will indicate the next index.
-    4. Initialize the last index in an array that points to NULL.
-    5. The member who represents the top of the list in the 
-        structure will point to the first free Icades which is Incodes 1.
-    */
+    char *runner = NULL;
+    size_t count = 0;
+    size_t i = 0;
+    fsp_ty *new_fs_pool = NULL;
+    
+    chunk_size = Alignment(chunk_size);
+
+    count = (memory_size - sizeof(fsp_ty)) / chunk_size;
+
+    new_fs_pool = (fsp_ty *)memory;
+    new_fs_pool->first_block = 0;
+
+    runner = (char *)(new_fs_pool->first_block + 1);
+
+    for (i = 0; i < count; i++)
+    {
+        *(size_t *)runner += chunk_size;
+
+        runner += chunk_size;
+    }
+    
+    *(size_t *)runner = ULONG_MAX;
 }
 
+/*  handle empty freelist */
+    
+    /*  save freelist[first_blk] address in local temp var */
+
+    /*  copy first_blk */
+
+    /*  progression: first_blk = next */
+
+    /*  convert copy to pointer and return */
 void *FSPoolAlloc(fsp_ty *fs_pool)
 {
-    /*
-    1. Sets a pointer and compares it to NULL for the return value "ret val".
-    2. Check if there is a space that can be allocated to the user.
-    3. If yes then the pointer for the return value 
-                            will point to the (fs_pool + OFFSETOF)[next_to_free].
-    4. The next place to FREE will be the 
-                            indexing dereference of what we send to the user.
-    5. free_chunks -= 1.
-    6. Returns to the user a pointer to the memory he requested.
-    */
+    assert(fs_pool);
+    char *runner = (char *)fs_pool;
+
+    if (0 == FSPoolCountFreeChunks(fs_pool))
+    {
+        return NULL;
+    }
+
+    runner = runner + sizeof(fs_pool);
+
+    fs_pool->first_block = *runner;
+
+    return runner; 
 }
 
+/*  assert given pointer is part of the pool"
+        chunk >= freelist && chunk <= (char *)freelist + pool_size  */
+    
+    /* copy first_blk into chunk */
+
+    /* point first_blk at chunk */
 void FSPoolFree(fsp_ty *fs_pool, void *chunk_to_free)
 {
-    /*
-    1. We will check if the pointer we got from the user is not equal to NULL.
-    2. Put the next place for release inside the pointer we got from the user.
-    3. free_chunks += 1.
-    4. It was made that the next place for release would be
-                         equal to the voter we received.
-    */
+    assert(fs_pool);
+    assert((chunk_to_free >= fs_pool) && ((char *)chunk_to_free <=
+                                     (char *)(fs_pool + fs_pool->first_block)));
+   
+    *(size_t *)chunk_to_free = fs_pool->first_block;
+
+    fs_pool->first_block = chunk_to_free;
 }
 
+/*  save first_blk to temp variable, create counter. */
 
+    /*  while temp isnt containing size_t max - dereference to that address */
+        
+    /*  update counter */
+
+    /*  prgression: save temp with current address data */
+
+    /*  when while ends, return count; */    
 size_t FSPoolCountFreeChunks(fsp_ty *fs_pool)
 {
-    /*
-    We return the member that represents the count.
-    */
+   size_t count = 0;
+   size_t tmp = fs_pool->first_block;
+
+   while (tmp != ULONG_MAX)
+   {
+       ++count;
+        tmp = *(size_t)(fs_pool + sizeof(fs_pool) + tmp);
+   }
+   
+   return count;
 }
 
+/* assert chunk_size > sizeof(size_t) */
+
+/*  calculate size of num_chunks * chunk_size + size of fsp_ty without 
+the pointer (offset until pointer) */
+
+/* need to make sure chunk_size starts at word boundary */
 size_t FSPoolCalcSize(size_t num_chunks, size_t chunk_size)
 {
-    /*
-    We will return the size of the structure plus the product of
-    num_chunks * chunk_size
-    */
+    assert(chunk_size > sizeof(size_t));
+
+    chunk_size = Alignment(chunk_size);
+       
+    size_t ret_val = (chunk_size * num_chunks) + sizeof(fsp_ty);
+
+    return ret_val;   
+}
+
+
+static Alignment(chunk_size)
+{
+    while (chunk_size % 8 != 0)
+    {
+        ++chunk_size;
+    }
+    return chunk_size;
 }
