b7380bf223d48fc22e48da2b81c380784e8ecfe9
Michael <michael24kolet@gmail.com>
2022-03-16 14:36:01 +0200

    updat

--- ststem_progrming/Threads_of_sadness/Threads_of_sadness.c
+++ ststem_progrming/Threads_of_sadness/Threads_of_sadness.c
@@ -7,20 +7,17 @@
 #define MAX_THREADS 100000
 
 
-typedef struct Threads_of_sadness
-{
-    size_t val;
-
-}threads_ty;
 
 /**********************************************/
 
 void PrintArr();
-void *PutIdx(void *ptr_strcut);
+void *PutIdx(void *ptr);
+void UpdatDividors(int num);
 
 /***********************************************/
 
 int arr[MAX_THREADS] = {0};
+int sum_of_divisors = 0;
 
 /**********************************************/
 
@@ -29,41 +26,76 @@ int arr[MAX_THREADS] = {0};
 int main()
 {
     pthread_t id[MAX_THREADS] = {0};
+    int *arg = NULL;
+    int **ptr = &arg;
     time_t start = 0;
     time_t end = 0;
-    threads_ty *th = (threads_ty *)malloc(sizeof(size_t));
+    
 
     size_t i = 0;
+    /* Creating uninitialize attribute*/
+	pthread_attr_t attr;
+	
+	/* Function to init attr to default so it can be set later to other
+	attribute */
+	pthread_attr_init (&attr);
+	
+	/* Set detach state for the created threads */
+	pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_DETACHED); 
 
     start = time(NULL);
+    
+          
+
     for (i = 0; i < MAX_THREADS; ++i)
     {
-        th->val = i;       
-        pthread_create(&id[i], NULL, PutIdx, th);
+        while (pthread_create(&id[i], &attr, PutIdx, (void *)i ) != 0)
+        {
+            continue;
+        }
+        
+        (void) pthread_join(id[i], NULL);
+
     }
     end = time(NULL);
 
-    sleep(5);
-
+    sleep(3);
+    /* Destroy attribute */
+	pthread_attr_destroy (&attr); 
     PrintArr();
     puts("\n");
     printf(" %ld\n", end - start);
     return 0;
 }
 
-void *PutIdx(void *ptr_strcut)
+void *PutIdx(void *ptr)
 {
-    threads_ty *data = (threads_ty *)ptr_strcut;
-    arr[data->val] = data->val;
-
+    int *data = (int *)&ptr;
+    arr[*data] = *data;
+    UpdatDividors(*data);
 }
 
 void PrintArr()
 {
     size_t i = 0;
 
-    for (i = 0; i < MAX_THREADS/2; ++i)
+    for (i = 0; i < MAX_THREADS; ++i)
     {
         printf("%d ,", arr[i]);
+    }
+}
+
+
+void UpdatDividors(int num)
+{
+    int i = 0;
+
+    for ( i = 0; i < num; ++i)
+    {
+        if (0 == num % i)
+        {
+            sum_of_divisors += i;
+        }
+        
     }
 }
