7d83947a73c16f449ffeb031f47a0d9f412e3409
Michael <michael24kolet@gmail.com>
2022-05-02 22:46:37 +0300

    MergeTwoList in c

--- quizzes/MergeTwoLists.c
+++ quizzes/MergeTwoLists.c
@@ -1,72 +1,137 @@
-#include <stdio.h>
-#include <assert.h> /*assert*/ 
-#include <stdlib.h> /*malloc free*/
-
-typedef struct ListNode
-	{
-      struct ListNode *next;
-      int data;
-
-	} node_ty;
-
-node_ty *GoNext(node_ty *node);
-node_ty *NewNode(int data); 
-node_ty *MergeTwoLists(node_ty *list_a, node_ty *list_b);
-void Insert(node_ty *prev, node_ty *curr);
-
-
-int main(void) 
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+ 
+/* Link list node */
+typedef struct Node 
 {
-	return 0; 
-}
-
-
-node_ty *MergeTwoLists(node_ty *list_a, node_ty *list_b) 
-{ 
-	/*def a new list dummy is the first node*/
-	node_ty *dummy = NewNode(0);
-	node_ty *curr = dummy;
-
-	/*while a and b in next is not null*/
-	while (list_a->next != NULL || list_b->next != NULL)
-	{
-		/*check who is small to start with*/
-		/*insert the small node to new list*/
-		/*if is from a then move to next in a*/
-		if(list_a->data <= list_b->data)
-		{
-			Insert(curr, list_a);
-			list_a = GoNext(list_a);
-		}
-		/*if is from b then move to next in b*/
-		else if(list_a->data >= list_b->data)
-		{
-			Insert(curr, list_b);
-			list_b = GoNext(list_b);
-		}
-	}
-
-	/*return the first node in the ret list*/
-	return dummy->next;
-}
-
-
-node_ty *NewNode(int data)
+    int data;
+    struct Node* next;
+	
+} node_ty;
+ 
+/* pull off the front node of the source and put it in dest */
+void MoveNode(struct Node** destRef, struct Node** sourceRef);
+ 
+/* Takes two lists sorted in increasing order, and splices
+   their nodes together to make one big sorted list which
+   is returned.  */
+struct Node* SortedMerge(struct Node* a, struct Node* b)
 {
-	node_ty *node = (node_ty *)malloc(sizeof(node_ty));
-
-	node->data = data;
-
-	return node;   	
+    /* a dummy first node to hang the result on */
+    struct Node dummy;
+ 
+    /* tail points to the last result node  */
+    struct Node* tail = &dummy;
+ 
+    /* so tail->next is the place to add new nodes
+      to the result. */
+    dummy.next = NULL;
+    while (1)
+    {
+        if (a == NULL)
+        {
+            /* if either list runs out, use the
+               other list */
+            tail->next = b;
+            break;
+        }
+        else if (b == NULL)
+        {
+            tail->next = a;
+            break;
+        }
+        if (a->data <= b->data)
+            MoveNode(&(tail->next), &a);
+        else
+            MoveNode(&(tail->next), &b);
+ 
+        tail = tail->next;
+    }
+    return(dummy.next);
 }
-
-void Insert(node_ty *prev, node_ty *curr)
+ 
+/* UTILITY FUNCTIONS */
+/* MoveNode() function takes the node from the front of the
+   source, and move it to the front of the dest.
+   It is an error to call this with the source list empty.
+ 
+   Before calling MoveNode():
+   source == {1, 2, 3}
+   dest == {1, 2, 3}
+ 
+   After calling MoveNode():
+   source == {2, 3}
+   dest == {1, 1, 2, 3} */
+void MoveNode(struct Node** destRef, struct Node** sourceRef)
 {
-	prev->next = curr;
+    /* the front source node  */
+    struct Node* newNode = *sourceRef;
+    assert(newNode != NULL);
+ 
+    /* Advance the source pointer */
+    *sourceRef = newNode->next;
+ 
+    /* Link the old dest off the new node */
+    newNode->next = *destRef;
+ 
+    /* Move dest to point to the new node */
+    *destRef = newNode;
 }
-
-
-node_ty *GoNext(node_ty *node)
+ 
+ 
+/* Function to insert a node at the beginning of the
+   linked list */
+void push(struct Node** head_ref, int new_data)
 {
-	return node->next;
+    /* allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+ 
+    /* put in the data  */
+    new_node->data  = new_data;
+ 
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);
+ 
+    /* move the head to point to the new node */
+    (*head_ref)    = new_node;
+}
+ 
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node!=NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+}
+ 
+/* Driver program to test above functions*/
+int main()
+{
+    /* Start with the empty list */
+    struct Node* res = NULL;
+    struct Node* a = NULL;
+    struct Node* b = NULL;
+ 
+    /* Let us create two sorted linked lists to test
+      the functions
+       Created lists, a: 5->10->15,  b: 2->3->20 */
+    push(&a, 15);
+    push(&a, 10);
+    push(&a, 5);
+ 
+    push(&b, 20);
+    push(&b, 3);
+    push(&b, 2);
+ 
+    /* Remove duplicates from linked list */
+    res = SortedMerge(a, b);
+ 
+    printf("Merged Linked List is: \n");
+    printList(res);
+	puts("\n");
+ 
+    return 0;
 }
